43c62b87ed70fcfdf6306ddc1ba42a1576edcf04
Erin Tyla Steenveld <st10258400@vcconnect.edu.za>
2024-04-02 15:16:41 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10258400_Erin_CLDV_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
